bff79a0ac869b033989a8c71cc38122138cc6d74
Jonathan Carter <jon@placidbits.com>
2020-10-17 12:40:43 -0400

    just lizard

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const discordToken = process.env.DISCORD_TOKEN;
 
 const getGeckoFace = async () => {
     try {
-        var response = await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${giphyKey}&tag=lizard+gecko`);
+        var response = await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${giphyKey}&tag=lizard`);
         return response.data.data.images.original.url;
     } catch (err) {
         console.error(err);
